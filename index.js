d26b7fd3ef139ac0900c4d314d651b6814571348
MD. Zaid <146553796+Zed69696969@users.noreply.github.com>
2025-02-05 12:28:46 +0530

    github api data represent

--- index.js
+++ index.js
@@ -2,7 +2,41 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
-
+const data = {
+    "login": "Zed69696969",
+    "id": 146553796,
+    "node_id": "U_kgDOCLw7xA",
+    "avatar_url": "https://avatars.githubusercontent.com/u/146553796?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Zed69696969",
+    "html_url": "https://github.com/Zed69696969",
+    "followers_url": "https://api.github.com/users/Zed69696969/followers",
+    "following_url": "https://api.github.com/users/Zed69696969/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Zed69696969/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Zed69696969/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Zed69696969/subscriptions",
+    "organizations_url": "https://api.github.com/users/Zed69696969/orgs",
+    "repos_url": "https://api.github.com/users/Zed69696969/repos",
+    "events_url": "https://api.github.com/users/Zed69696969/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Zed69696969/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "zed",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 6,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2023-09-30T13:43:33Z",
+    "updated_at": "2025-01-24T05:59:51Z"
+  }
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,6 +45,9 @@ app.get('/youtube',(req,res) => {
 })
 app.get('/twitter',(req,res) => {
     res.send('asdf')
+})
+app.get('/git',(req, res) => {
+    res.json(data)
 })
 app.get('/login', (req,res) => {
     res.send('<h1>this is a logni test</h1>')
